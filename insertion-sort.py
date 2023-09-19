19b4d05461389dffaab75bb9cfa761fb8ff770a7
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2023-09-18 22:26:59 -0400

    Update insertion-sort.py

--- insertion-sort.py
+++ insertion-sort.py
@@ -3,22 +3,48 @@
 #   Steven Kyritsis     #
 #   9-22-23             #
 #   Insertion sort with #
-#   a runtime timer     #
-#########################
-import time
-start = time.time()
+#  a comparison counter #
+#########################\
+
+#1. import random from numy to generate random number
+from numpy import random
+
+#2. define insertion sort function
 def insert_sort(a, n):
+    compcount = 0
     for i in range(1,n):
         j=i
         while(j>0 and a[j]<a[j-1]):
+            compcount += 1
             temp = a[j]
             a[j] = a[j-1]
             a[j-1] = temp
             j=j-1
-    return a
+    return a, compcount
 
-arr = [70, 40, 1, 14, 53, 21, 50, 98, 39, 3, 17, 30, 8, 93, 51, 83, 35, 18, 91, 54, 100, 75, 67, 41, 73, 61, 66, 92, 74, 85, 57, 29, 2, 72, 69, 12, 76, 31, 43, 22, 19, 58, 99, 27, 16, 42, 44, 25, 15, 96]
+# Creating the array [n=32]
+arr = []
+for i in range(1,33):
+    arr.append(i)
+
+# Test for best case [n=32]
 print("Input array:\t", arr)
-print("Sorted array:\t", insert_sort(arr,len(arr)))
-end = time.time()
-print("Runtime: ",end-start)
+sorted, count = insert_sort(arr,len(arr))
+print("Sorted array:\t", sorted,
+      "\nBest case comparisons:\t", count, "\n")
+
+# Test for worst case [n=32]
+arr.reverse()
+print("Input array:\t", arr)
+sorted, count = insert_sort(arr,len(arr))
+print("Sorted array:\t", sorted,
+      "\nWorst case comparisons:\t", count, "\n")
+
+# Test for average case [n=32]
+arr = random.randint(32, size=(32)) # Testing with random integers between 1-32
+print("Input array:\t", arr)
+sorted, count = insert_sort(arr,len(arr))
+print("Sorted array:\t", sorted,
+      "\nAverage case comparisons:\t", count, "\n")
+
+# Test 
